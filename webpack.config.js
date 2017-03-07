2a71b89d8290c67341823d98eec386596d92fa95
Teddy Ortega <tortega128@gmail.com>
2017-03-07 11:37:59 -0500

    move demo here from component repository

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry:  './src/index.js',
+    output: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test:   /\.js/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new CopyWebpackPlugin([
+            { from: 'src/favicon.ico' }
+        ])
+    ]
+};
